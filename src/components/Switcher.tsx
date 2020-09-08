ab9a6fc44bcac510c03ad9f6b927e999d5e9e3d8
darya-markova <darya.towarddreams@gmail.com>
2020-09-09 01:13:11 +0300

    decorating layouts

--- src/components/Switcher.tsx
+++ src/components/Switcher.tsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { TemplateType } from "../templates";
 import { Card, Row, Col } from "antd";
+import "./Switcher.css";
 
 export const Switcher = (props: {
   onTemplateSwitched: (type: TemplateType) => void;
 }) => {
+  const [selectedIndex, setSelectedIndex] = useState<number>(0);
+  const templateSwitched = (index: number) => {
+    setSelectedIndex(index);
+    props.onTemplateSwitched(index);
+  };
+
   return (
     <div className="app-switcher">
       <Row justify="center" gutter={18}>
@@ -12,17 +19,11 @@ export const Switcher = (props: {
           .filter((key) => +key >= 0)
           .map((key: string, index: number) => (
             <Col style={{ marginBottom: "14px" }}>
-              <div style={{ textAlign: "center", color: "#fff" }}>
-                {TemplateType[index]}
-              </div>
+              <div className="title">{TemplateType[index]}</div>
               <Card
+                className={index === selectedIndex ? "tile active" : "tile"}
                 size="small"
-                style={{
-                  width: "110px",
-                  height: "140px",
-                  cursor: "pointer",
-                }}
-                onClick={() => props.onTemplateSwitched(index)}
+                onClick={() => templateSwitched(index)}
                 bordered={true}
               ></Card>
             </Col>
