ef6f349bf3f0b69bb4f4dc524011da7b40275095
darya-markova <darya.towarddreams@gmail.com>
2020-09-14 00:02:13 +0300

    reformatting with prettier

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const fs = require("fs");
 const pdf = require("html-pdf");
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(cors());
@@ -14,27 +14,27 @@ app.use(cors());
 const port = 3005;
 
 // downloading via pure get method
-app.get("/pdf", (req, res) => {	
-	res.download("./resume.pdf");
+app.get("/pdf", (req, res) => {
+  res.download("./resume.pdf");
 });
 
 // just test script
 // https://www.npmjs.com/package/html-pdf
 app.post("/pdf", (req, res) => {
   try {
-		//TODO: escape all double quotes req.body.content 
-	  const html = `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>Resume</title></head><body>${req.body.content}</body></html>`;
-		// const html = fs.readFileSync('./temp.html', 'utf8');
-		pdf.create(html, {}).toFile("./resume.pdf", function (err, response) {
+    //TODO: escape all double quotes req.body.content
+    const html = `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>Resume</title></head><body>${req.body.content}</body></html>`;
+    // const html = fs.readFileSync('./temp.html', 'utf8');
+    pdf.create(html, {}).toFile("./resume.pdf", function (err, response) {
       if (err) {
-				console.log(err);
+        console.log(err);
         return res.status(500).json(err);
-			}
-			
-			res.download("./resume.pdf");
+      }
+
+      res.download("./resume.pdf");
     });
-  } catch (err) {	
-		console.log(err);
+  } catch (err) {
+    console.log(err);
     res.status(500).json(err);
   }
 });
--- frontend/src/pages/ResumeEditor/ResumeEditorPage.tsx
+++ frontend/src/pages/ResumeEditor/ResumeEditorPage.tsx
@@ -3,25 +3,30 @@ import { Editor, Viewer } from "../../components";
 import { SlackOutlined } from "@ant-design/icons";
 import { Button } from "antd";
 import { Link } from "react-router-dom";
-import axios from 'axios';
-import fileDownload from 'js-file-download';
+import axios from "axios";
+import fileDownload from "js-file-download";
 import "./ResumeEditorPage.css";
 
-
 export function ResumeEditorPage() {
-	const onDownloadPdfButtonClicked = () => {
-		axios.post('http://localhost:3005/pdf', {
-			"content": "<div style=\"text-align:center; border:1px dotted orange\"><h2>Love love love</h2></div>"
-		}, {
-			responseType: 'blob'
-		}) 
-		/*axios
+  const onDownloadPdfButtonClicked = () => {
+    axios
+      .post(
+        "http://localhost:3005/pdf",
+        {
+          content:
+            '<div style="text-align:center; border:1px dotted orange"><h2>Love love love</h2></div>',
+        },
+        {
+          responseType: "blob",
+        }
+      )
+      /*axios
 			.get('http://localhost:3005/pdf', {
 				responseType: 'blob'
 			}) */
-			.then(response => fileDownload(response.data, 'resume.pdf'))
-			.catch(err => console.log(err));
-	};
+      .then((response) => fileDownload(response.data, "resume.pdf"))
+      .catch((err) => console.log(err));
+  };
 
   return (
     <div className="resume-editor-page">
@@ -40,7 +45,13 @@ export function ResumeEditorPage() {
             <SlackOutlined />
             <span>Select template</span>
           </Link>
-					<Button type="primary" onClick={onDownloadPdfButtonClicked} style={{marginLeft: "12px"}}>Download pdf</Button>
+          <Button
+            type="primary"
+            onClick={onDownloadPdfButtonClicked}
+            style={{ marginLeft: "12px" }}
+          >
+            Download pdf
+          </Button>
         </div>
       </div>
     </div>
