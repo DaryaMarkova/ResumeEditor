3d6c0eb28d72ad7181fe870f7542fdf0201d99a6
darya-markova <darya.towarddreams@gmail.com>
2020-09-13 23:48:49 +0300

    excluding frontend module

---
+++ backend/src/index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const fs = require("fs");
+const pdf = require("html-pdf");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
+app.use(cors());
+
+const port = 3005;
+
+// downloading via pure get method
+app.get("/pdf", (req, res) => {	
+	res.download("./resume.pdf");
+});
+
+// just test script
+// https://www.npmjs.com/package/html-pdf
+app.post("/pdf", (req, res) => {
+  try {
+		//TODO: escape all double quotes req.body.content 
+	  const html = `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><title>Resume</title></head><body>${req.body.content}</body></html>`;
+		// const html = fs.readFileSync('./temp.html', 'utf8');
+		pdf.create(html, {}).toFile("./resume.pdf", function (err, response) {
+      if (err) {
+				console.log(err);
+        return res.status(500).json(err);
+			}
+			
+			res.download("./resume.pdf");
+    });
+  } catch (err) {	
+		console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
