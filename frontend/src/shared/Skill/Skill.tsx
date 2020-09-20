24f7aba287df3b203b0d9a6c78d25915f1d244a5
darya-markova <darya.towarddreams@gmail.com>
2020-09-20 22:27:21 +0300

    skill component fix

--- frontend/src/shared/Skill/Skill.tsx
+++ frontend/src/shared/Skill/Skill.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
-import { Collapse, Typography, Row, Col, Radio } from "antd";
+import { Collapse, Typography, Row, Col, Button } from "antd";
 import { EditableInput } from "../Input/Input";
 import { Skill, SkillLevelType } from "../../types";
-import { RadioChangeEvent } from "antd/lib/radio";
+import { StarOutlined, StarFilled, DeleteOutlined } from "@ant-design/icons";
 import "./Skill.css";
 
 export const EditableSkill = () => {
@@ -15,8 +15,8 @@ export const EditableSkill = () => {
     setSkill({ ...skill, [bindProperty]: value });
   };
 
-  const onSelectedSkillChanged = (event: RadioChangeEvent) => {
-    setSkill({ ...skill, level: (SkillLevelType as any)[event.target.value] });
+  const onSelectedSkillChanged = (levelName: string) => {
+    setSkill({ ...skill, level: (SkillLevelType as any)[levelName] });
   };
 
   const levelNames = Object.keys(SkillLevelType).filter(
@@ -27,47 +27,85 @@ export const EditableSkill = () => {
   const { Text } = Typography;
 
   return (
-    <Collapse className="widget-skill" style={{ width: "100%" }}>
-      <Panel
-        className="widget-skill-panel"
-        header={
-          <>
-            <div className="widget-skill-name">(Not specified)</div>
-            <Text className="widget-skill-level" type="secondary">
-              Level
-            </Text>
-          </>
-        }
-        key="1"
+    <>
+      <Row
+        className="full-width"
+        gutter={10}
+        align="top"
+        justify="space-between"
       >
-        <Row gutter={24}>
-          <Col span={12}>
-            <EditableInput
-              onInputValueChanged={onSkillNameChanged}
-              placeholder="Skill"
-              defaultValue={skill.skillName}
-              bindProperty="skillName"
-            />
-          </Col>
-          <Col>
-            <div style={{ paddingTop: "4px" }}>
-              <Text className="widget-skill-level-text" type="secondary">
-                Level&nbsp;&mdash;&nbsp;
-                {SkillLevelType[skill.level]}
-              </Text>
-              <Radio.Group onChange={onSelectedSkillChanged}>
-                {levelNames.map((level, index) => (
-                  <Radio.Button
-                    className="widget-skill-level-btn"
-                    key={index}
-                    value={level}
-                  ></Radio.Button>
-                ))}
-              </Radio.Group>
-            </div>
-          </Col>
-        </Row>
-      </Panel>
-    </Collapse>
+        <Col span={23}>
+          <Collapse
+            bordered={true}
+            className="widget-skill"
+            style={{ width: "100%" }}
+          >
+            <Panel
+              className="widget-skill-panel"
+              header={
+                <>
+                  <div className="widget-skill-name">(Not specified)</div>
+                  <Text className="widget-skill-level" type="secondary">
+                    {SkillLevelType[skill.level]}
+                  </Text>
+                </>
+              }
+              key="1"
+            >
+              <Row gutter={24}>
+                <Col span={12}>
+                  <EditableInput
+                    onInputValueChanged={onSkillNameChanged}
+                    placeholder="Skill"
+                    defaultValue={skill.skillName}
+                    bindProperty="skillName"
+                  />
+                </Col>
+                <Col>
+                  <div style={{ paddingTop: "4px" }}>
+                    <Text className="widget-skill-level-text" type="secondary">
+                      Level&nbsp;&mdash;&nbsp;
+                      {SkillLevelType[skill.level]}
+                    </Text>
+                    <div className={`widget-skill-level-active-` + skill.level}>
+                      {levelNames.map((level, index) => (
+                        <Button
+                          onClick={() => onSelectedSkillChanged(level)}
+                          key={index}
+                          value={level}
+                          size="small"
+                          shape="circle"
+                          type="text"
+                          className="widget-skill-level-icon"
+                          icon={
+                            index <= skill.level ? (
+                              <StarFilled
+                                style={{ color: "rgb(33, 150, 243)" }}
+                              />
+                            ) : (
+                              <StarOutlined
+                                style={{ color: "rgb(33, 150, 243)" }}
+                              />
+                            )
+                          }
+                        />
+                      ))}
+                    </div>
+                  </div>
+                </Col>
+              </Row>
+            </Panel>
+          </Collapse>
+        </Col>
+        <Col span={1}>
+          <Button
+            shape="circle"
+            type="text"
+            size="small"
+            icon={<DeleteOutlined />}
+          />
+        </Col>
+      </Row>
+    </>
   );
 };
