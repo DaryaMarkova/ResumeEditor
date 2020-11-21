0d2ab2086b0bd4a010f55e6cd4bf8a2fac0b0521
darya-markova <darya.towarddreams@gmail.com>
2020-11-21 21:12:28 +0300

    cancelling previous requests via axios

--- frontend/src/components/Viewer/Viewer.tsx
+++ frontend/src/components/Viewer/Viewer.tsx
@@ -10,92 +10,98 @@ import { useStore } from "../../utils/useStore";
 import { useDebounce } from "../../utils/useDebounce";
 import { Document, Page } from "react-pdf";
 import { pdfjs } from "react-pdf";
-import axios from "axios";
-import {
-  PDFDocumentProxy,
-  PDFPageProxy,
-  PDFRenderParams,
-  PDFRenderTask,
-} from "pdfjs-dist";
-import { render } from "@testing-library/react";
+import axios, { CancelTokenSource } from "axios";
+import { PDFDocumentProxy, PDFRenderTask } from "pdfjs-dist";
+
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+const CancelToken = axios.CancelToken;
+let source: CancelTokenSource | null = null;
 
 export const Viewer = (props: { templateRef?: RefObject<HTMLDivElement> }) => {
   const { store } = useStore();
-
-  const [iframeKey, setIframeKey] = useState<number>(new Date().getTime());
   const [countPages, setCountPages] = useState(1);
+  const [pageNumber, setPageNumber] = useState(1);
 
-  const pageNumber = 1;
   const debouncedStore = useDebounce(store.profile, 300);
   const documentRef = useRef<HTMLDivElement>(null);
-  const canvasRef = useRef<any>(null); // bad approach
+  const canvasRef = useRef<HTMLCanvasElement>(null); // bad approach
 
   let renderTask: PDFRenderTask | null = null;
 
-  async function loadPdf() {
+  useEffect(() => {
+    (async function () {
+      const pdf = await loadPdfDocument();
+
+      if (pdf) {
+        setCountPages(pdf?.numPages);
+      }
+    })();
+  }, []);
+
+  useEffect(() => {
+    updatePdfDocument();
+  }, [debouncedStore]);
+
+  async function loadPdfDocument(): Promise<PDFDocumentProxy | undefined> {
     if (renderTask) {
       renderTask.cancel();
     }
-
     // if (!renderTask?.promise.isRejected) {
     //   return;
     // }
-
-    // const canvas = document.createElement("canvas");
     const pdf = await pdfjs.getDocument("/resume.pdf").promise;
     const page = await pdf.getPage(pageNumber);
     const viewport = page.getViewport({ scale: 0.8 });
-
     const canvas = canvasRef.current;
-    const context = canvas.getContext("2d");
+    const context = canvas?.getContext("2d");
+
+    if (!canvas || !context) {
+      return;
+    }
 
     canvas.height = viewport.height;
     canvas.width = viewport.width;
 
-    if (context) {
-      const renderContext = {
-        canvasContext: context,
-        viewport: viewport,
-      };
+    const renderContext = {
+      canvasContext: context,
+      viewport: viewport,
+    };
 
-      renderTask = page.render(renderContext);
+    renderTask = page.render(renderContext);
 
-      renderTask.promise.then(function () {
-        console.log("pdf has been reloaded");
-      }, loadPdf);
+    renderTask.promise.then(() => {}, loadPdfDocument);
 
-      // documentRef.current?.childNodes.forEach((child) => child.remove());
-      // documentRef.current?.appendChild(canvas);
-    }
+    return pdf;
   }
 
-  useEffect(() => {
-    loadPdf();
-  }, []);
+  const updatePdfDocument = () => {
+    if (source) {
+      source.cancel("Cancel previous request");
+    }
 
-  useEffect(() => {
-    generatePdf();
-  }, [debouncedStore]);
+    source = CancelToken.source();
 
-  const generatePdf = () => {
-    // cancel axios request if newone is upcoming
     axios
-      .post("/render_pdf", {
-        firstName: store.profile.firstName,
-        lastName: store.profile.lastName,
-        summary: store.profile.summary,
-      })
+      .post(
+        "/render_pdf",
+        {
+          firstName: store.profile.firstName,
+          lastName: store.profile.lastName,
+          summary: store.profile.summary,
+        },
+        {
+          cancelToken: source.token,
+        }
+      )
       .then(async (response) => {
-        // await renderPdfToCanvas();
-        // setIframeKey(new Date().getTime());
-        loadPdf();
+        const pdf = await loadPdfDocument();
+
+        if (pdf) {
+          setCountPages(pdf?.numPages);
+        }
       })
       .catch((err) => console.log(err));
-  };
-
-  const onLoadSuccess = (pdf: PDFDocumentProxy) => {
-    setCountPages(pdf.numPages);
   };
 
   return (
@@ -108,45 +114,5 @@ export const Viewer = (props: { templateRef?: RefObject<HTMLDivElement> }) => {
     >
       <canvas ref={canvasRef}></canvas>
     </div>
-  );
-
-  return (
-    <>
-      <div
-        style={{
-          width: "fit-content",
-          margin: "16px auto",
-        }}
-      >
-        <div ref={documentRef}>
-          <Document
-            file={"/resume.pdf"}
-            key={iframeKey}
-            // loading={() => {
-            //   const childNode = documentRef.current?.childNodes[0];
-            //   const cloneNode = childNode?.cloneNode(true);
-
-            //   return (
-            //     <div
-            //       ref={(nodeElement) => {
-            //         nodeElement &&
-            //           cloneNode &&
-            //           nodeElement.appendChild(cloneNode);
-            //       }}
-            //     ></div>
-            //   );
-            // }}
-            onLoadSuccess={onLoadSuccess}
-          >
-            <Page pageNumber={1} />
-          </Document>
-        </div>
-      </div>
-    </>
-    // <iframe
-    //   key={iframeKey}
-    //   src="http://localhost:3005/resume.pdf"
-    //   style={{ width: "100%", height: "100%" }}
-    // />
   );
 };
